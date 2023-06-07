b9f06c00ce1ced5e452858b9cb69cb0da2ecf984
James Fiddament-Harris <89358744+DrFidd@users.noreply.github.com>
2023-06-07 16:44:55 +0100

    Update config.js
    Changed links

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Stable Diffusion',
+			icon: 'brain-circuit',
+			link: 'http://127.0.0.1:7860/',
 		},
 		{
 			id: '6',
